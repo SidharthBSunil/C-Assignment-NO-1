33976158c3a12f35611d83a1bbbc056f026d44c3
SidharthBsunil <sidharthbsunil@gmail.com>
2026-07-22 19:25:54 +0530

    fibo

--- Question_No_4_Fibnonacci.c
+++ Question_No_4_Fibnonacci.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
-{   int num;
-    printf("enter number to find fibnonacci");
+{   int num,a=0,b=1,res=0;
+    printf("enter number to find fibnonacci\n");
     scanf("%d",&num);
+    for(int i=0;i<=num;i++)
+    {
+        res=a+b;
+        printf("%d,\t",res);
+        a=b;
+        b=res;
+    }
 }
