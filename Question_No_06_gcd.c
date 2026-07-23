3955cc7d5d941062b1b6d145c0d3b613373f7b2d
SidharthBsunil <sidharthbsunil@gmail.com>
2026-07-23 17:47:54 +0530

    gcd_with resurionn

---
+++ Question_No_06_gcd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int gcd(int a ,int b)
+{   
+    int rem;
+    rem=a%b;
+    if(rem==0)
+    {
+        return b;
+    }
+    return(gcd(b,rem));
+
+}
+void main()
+{
+    int num1,num2;
+    printf("enter the two number to find gcd\n");
+    scanf("%d%d",&num1,&num2);
+    printf("gcd of number is %d\n",gcd(num1,num2));
+
+}
