25280737bcc4334d4817f31fe1b64edcd352b9f8
SidharthBsunil <sidharthbsunil@gmail.com>
2026-07-28 20:45:42 +0530

    re

---
+++ Question_no_13_reverse.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[20];
+    printf("enter the name to print reverse");
+    scanf("%s",str);
+    int n=strlen(str)-1;
+    int *
+}
