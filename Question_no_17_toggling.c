4b4730d185d3daf411aaa7c8e392ab56de35ed13
SidharthBsunil <sidharthbsunil@gmail.com>
2026-08-04 22:03:33 +0530

    bit manipulation

--- Question_no_17_toggling.c
+++ Question_no_17_toggling.c
@@ -22,13 +22,17 @@ void base_conversion(int digit,char *arr)
         i++;
         
     }
-    n=i-1;
-    for(int i=n;i>=0;i--)
-    {
-        printf("%c",*(arr+i));
-    }
 
-   printf("\n");
+    n=i-1;
+    printf("number in binary version\n");
+
+
+    for(int i=n;i>=0;i--)
+        {
+            printf("%c",*(arr+i));
+        }
+
+    printf("\n");
     
     
 
@@ -37,7 +41,23 @@ void base_conversion(int digit,char *arr)
 int main()
 {
     char* arr=(char*)malloc(32*sizeof(char));
-    base_conversion(7,arr);
+    int digit,position,no_of_bits,result=0;
+    //base_conversion(7,arr);
+    printf("enter the number ");
+    scanf("%d",&digit);
+    base_conversion(digit,arr);
+
+    printf("enter the staring bit");
+    scanf("%d",&position);
+
+    printf("number of bits\n");
+    scanf("%d",&no_of_bits);
+
+    digit ^=((1<<no_of_bits)-1)<<position;
+    printf("result is ");
+    
+    base_conversion(digit,arr);
+
 
 
 }
